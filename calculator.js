57b22b0fcbef548de606acd10cb12789606d3672
TheophilusDadzie <tkdadzie@st.ug.edu.gh>
2022-06-30 17:04:29 -0700

    calculator

--- calculator.js
+++ calculator.js
@@ -14,6 +14,26 @@ function calculate(){
     if(buttonText==="AC"){
         output.innerText = "" ;
         result.innerText = "0" ;
+        result.style.animation = "";
+        output.style.animation = "";
+        return;
+    }
+
+    if(buttonText == "DEL"){
+        output.textContent = output.textContent.substring(0,output.textContent.length-1);
+        return;
+    }
+
+    if(buttonText === "="){
+        result.innerText = eval(output.innerText);
+        result.style.animation = "big 0.5s ease-in-out";
+        output.style.animation = "small 0.5s ease-in-out";
+        result.style.animationFillMode = "forwards";
+        output.style.animationFillMode = "forwards";
+        return;
+    } else {
+        output.textContent += buttonText;
+        return;
     }
 }
 
